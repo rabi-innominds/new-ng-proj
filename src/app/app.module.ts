d7c7c021d12d8232fe1ee9f994b45d2f07e6f48b
Rabi Kiran <rsrirangam@innominds.com>
2025-02-06 15:04:35 -0600

    Updated to Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,12 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
     PokemonListComponent,
     PokemonDetailsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], imports: [BrowserModule,
+    AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())]
 })
 export class AppModule { }
